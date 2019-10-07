ff17f19b1314802256c63a94a723d27fff30d740
Derek Butler <derekcbutler@gmail.com>
2019-10-07 15:30:47 -0600

    yup

--- src/Components/Purchased/Purchased.js
+++ src/Components/Purchased/Purchased.js
@@ -1,18 +1,64 @@
 import React from "react";
 import "./Purchased.css";
 
-function Purchased(props) {
-  return (
-    <div id="body">
-      <div className="row-one">
-        <div className="row1-box">
-            <button className="delete" onClick={() => props.handleEdit(props.e.name)}>Edit  </button>
-            {props.e.name}
-           <button className="delete" onClick={() => props.deleteButton(props.id, props.e.name)}>  Delete</button>
+class Purchased extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      editing: false,
+      input: ""
+    };
+  }
+
+  edit = () => {
+    this.setState({
+        editing: !this.state.editing
+    })
+
+  }
+
+  onChange= e => {
+    let {name, value} = e.target
+    this.setState({
+        [name]: value
+    })
+  }
+
+  render() {
+      console.log(this.state)
+    return (
+      <div id="body">
+        <div className="row-one">
+          {this.state.editing ? (
+            <div className="row1-box">
+              <input name='input' value={this.state.input} onChange={(e) => this.onChange(e)} />
+              <button>save</button>
+            </div>
+          ) : (
+            <div className="row1-box">
+              <button
+                className="delete"
+                onClick={() => this.props.handleEdit(this.props.e.name)}
+              >
+                Edit{" "}
+              </button>
+              {/* <input /> */}
+              {this.props.e.name}
+              <button
+                className="delete"
+                onClick={() =>
+                  this.props.deleteButton(this.props.id, this.props.e.name)
+                }
+              >
+                {" "}
+                Delete
+              </button>
+            </div>
+          )}
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Purchased;
