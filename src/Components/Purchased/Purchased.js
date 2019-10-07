68dbd002a3b7f78842c282b9f93cb76aa02dc63c
Derek Butler <derekcbutler@gmail.com>
2019-10-07 15:18:42 -0600

    basically finished

--- src/Components/Purchased/Purchased.js
+++ src/Components/Purchased/Purchased.js
@@ -6,8 +6,9 @@ function Purchased(props) {
     <div id="body">
       <div className="row-one">
         <div className="row1-box">
+            <button className="delete" onClick={() => props.handleEdit(props.e.name)}>Edit  </button>
             {props.e.name}
-           <button className="delete" onClick={() => props.deleteButton(props.id, props.e.name)}>Delete</button>
+           <button className="delete" onClick={() => props.deleteButton(props.id, props.e.name)}>  Delete</button>
         </div>
       </div>
     </div>
